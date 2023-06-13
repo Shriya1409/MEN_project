92a195d0dc5a9be7845391237d03ce68aac9d87e
Priya <sopanayirp@gmail.com>
2023-06-14 00:17:16 +0530

    final plcmt display changes

--- route/frontend/header.js
+++ route/frontend/header.js
@@ -2,6 +2,7 @@ let express =  require('express')
 let pageModel = require('../../model/pageModel')
 let deptModel = require('../../model/deptModel')
 let ITdeptModel = require('../../model/itdeptModel')
+let plcmtModel=require('../../model/plcmtModel')
 let router = express()
 
 deptModel.find({})
@@ -83,7 +84,6 @@ router.get('/', (req,res) => {
           res.render('../views/frontend/dynamic-page',{x})
         } else if(extractValue === 'register') {
             res.render('../views/frontend/register',{x})
-
         } 
         else {
           res.render('../views/frontend/dynamic-page',{x})
@@ -99,6 +99,25 @@ router.get('/', (req,res) => {
 
  })
 
+ router.get('/placement/:plcmt', (req,res) => {
+    plcmtModel.findOne({plcmtUrl: req.params.id})
+    .then((x) => {
+       if(x){
+        const extractValue = x.plcmtUrl;
+        if(extractValue === 'placement.ejs') {
+          res.render('../views/frontend/dynamic-plcmt',{x})
+        } 
+        
+       else {
+        res.redirect('/')
+       }
+    }
+    })
+    .catch((y) => {
+        console.log(y)
+    })
+    
+ })
 
 //  router.get('/:id', (req,res) => {
     //         pageModel.findOne({pageUrl: req.params.id})
