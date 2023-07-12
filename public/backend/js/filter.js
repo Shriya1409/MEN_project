64e59f96c2813e2b04d9385f059b6ffef5d6e249
Shriya Nadkarni <nadkarnishriya@gmail.com>
2023-07-12 17:39:20 +0530

    Syllabus & Scheme

--- public/backend/js/filter.js
+++ public/backend/js/filter.js
@@ -1,23 +1,15 @@
-function filterTable() {
-    var rc = document.getElementById('rcFilter');
-    var selectedRC=rc.value;
-    var semester = document.getElementById('semesterFilter');
-    var selectedSemester=semester.value;
-    var table = document.querySelectorAll('#tableBody tr');
-   
-    var rows=table.getElementByTagName("tr");
-  resultdata.forEach((t)=> {
-      var rc = row.getAttribute('resultdata-rc');
-      var semester = row.getAttribute('resultdata-sem');
-  
-      if (
-        (selectedRC === 'all' || selectedRC === rc) &&
-        (selectedsemester === 'all' || selectedsemester === semester)
-      ) {
-        row.style.display = 'table-row';
-      } else {
-        row.style.display = 'none';
-      }
-    });
-  }
-       
+
+  var $table = $('#table');
+    $(function () {
+        $('#toolbar').find('select').change(function () {
+            $table.bootstrapTable('refreshOptions', {
+                exportDataType: $(this).val()
+            });
+        });
+    })
+
+		var trBoldBlue = $("table");
+
+	$(trBoldBlue).on("click", "tr", function (){
+			$(this).toggleClass("bold-blue");
+	});     
