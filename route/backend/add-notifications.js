de00ee47fde90d8afa631497283db27700a61453
Priya <sopanayirp@gmail.com>
2023-07-18 23:42:12 +0530

    notification display

--- route/backend/add-notifications.js
+++ route/backend/add-notifications.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let multer = require('multer')
-let notificationsModel = require('../../model/notificationModel')
+let notificationModel = require('../../model/notificationModel')
 let router = express();
 
 // storage & file name setting
@@ -29,23 +29,29 @@ router.get('/', (req,res) => {
         res.render('../views/backend/add-notif')
     })
 
-router.post('/', upload.single('noticeinfo') , upload.single('usefulinfo'), (req,res) => {
-    notificationsModel.findOne({date: req.body.date})
+router.post('/', upload.fields([
+    { name: 'noticeinfo', maxCount: 1 },
+    { name: 'usefulinfo', maxCount: 1 }
+  ]), (req,res) => {
+    notificationModel.findOne({date: req.body.date})
     .then((a) => {
+        // const noticeinfo=req.files.map(file=>file.noticeinfo);
+        // const usefulinfo=req.files.map(file=>file.usefulinfo);
+
         if(a) {
             req.flash('err', 'Url already exists, Please try with another url!!')
             res.redirect('/notifications/')
             // console.log('Url already exists, Please try with another url!!')
         } else {
 
-            if(req.files && req.files>0) {
+            if(req.files) {
 
-                notificationsModel.create({
+                notificationModel.create({
                     date: req.body.date,
                     noticename:req.body.noticename,
-                    noticeinfo:req.file.filename,
+                    noticeinfo:req.body.filename,
                     usefulinfoname: req.body.usefulinfoname,
-                    usefulinfo: req.file.filename,
+                    usefulinfo: req.body.filename,
                 
                 })
                 .then((x) => {
@@ -53,7 +59,8 @@ router.post('/', upload.single('noticeinfo') , upload.single('usefulinfo'), (req
                      res.redirect('/notifications')
                 })
         
-            // } else {
+             } 
+            //  else {
         
             //     notificationsModel.create({
             //         course: req.body.course,
@@ -64,11 +71,11 @@ router.post('/', upload.single('noticeinfo') , upload.single('usefulinfo'), (req
             //         pageDetails: req.body.page_Details
             //     })
             //     .then((x) => {
-            //         req.flash('success', 'Your data has been added successfully')
-            //          res.redirect('/notifications')
+            // req.flash('success', 'Not added any data')
+            // //          res.redirect('/notifications')
             //     })
         
-            }
+            
 
         }
     })
